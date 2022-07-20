e68148f413b8b80f9377d3687f0cd5247a29cdb2
Raghu Daksh <raghudaksh9@gmail.com>
2022-07-19 21:49:37 -0700

    vivo
    vivo clone

--- app.js
+++ app.js
@@ -1,33 +1,25 @@
-const hamburger = document.querySelector('.hamburger');
-const navbar = document.querySelector('.navItems');
+
+
+window.addEventListener('scroll' , function(){
+   var header = document.querySelector('nav');
+   header.classList.toggle('sticky', window.scrollY > 0) ;
+    
+})
+
+let hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links ');
+const lists = document.querySelectorAll('.nav-links li');
+const toggleHamburger = document.querySelectorAll('.hamburger .line');
+
 
 hamburger.addEventListener('click', ()=>{
-    navbar.classList.toggle('active');
+ 
+   navLinks.classList.toggle('sidebar');
+   lists.forEach(list=>{
+      list.classList.toggle('fade');
+   })
+   toggleHamburger.forEach(line=>{
+      line.classList.toggle('cross');
+   })
 
 })
-// product page javascript
-const productPage = document.querySelector('#product-img');
-const smallImages = document.querySelectorAll('.small-img');
-
-smallImages.forEach(smallImage =>{
-    smallImage.addEventListener('click', ()=>{
-        productPage.src = smallImage.src;
-    })
-})
-
-// contact page
-
-const loginForm = document.querySelector("#login-form");
-const registerForm =  document.querySelector('#register-form');
-const indicator =  document.querySelector('#indicator');
-
- const register =()=>{
-     loginForm.style.transform = "translatex(0)";
-     registerForm.style.transform = "translatex(0)";
-     indicator.style.transform = "translatex(100px)"
- }
- const login =()=>{
-     loginForm.style.transform = "translatex(100%)";
-     registerForm.style.transform = "translatex(100%)";
-     indicator.style.transform = "translatex(0)"
- }
